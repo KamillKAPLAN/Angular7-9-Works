d677896877996db2aa3ed0d87d3a28d212e00755
KamillKAPLAN <kamilkaplnn@gmail.com>
2019-06-10 11:31:00 +0300

    employees(işçi) modeli ve componenti oluşturuldu

---
+++ src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ListEmployeesComponent } from './employees/list-employees.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ListEmployeesComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
