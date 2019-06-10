e315fb6a3ce0a1e240dee7ac73f62d032d0b0ac1
KamillKAPLAN <kamilkaplnn@gmail.com>
2019-06-10 13:41:04 +0300

    employees(işçi) create-employee componenti ve router oluşturuldu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
+import { CreateEmployeeComponent } from './employees/create-employee.component';
+
+const appRoutes : Routes = [
+  { path : 'list', component : ListEmployeesComponent },
+  { path : 'create', component : CreateEmployeeComponent },
+  { path : '', redirectTo: '/list', pathMatch: 'full' }
+]
 
 @NgModule({
   declarations: [
     AppComponent,
-    ListEmployeesComponent
+    ListEmployeesComponent,
+    CreateEmployeeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
