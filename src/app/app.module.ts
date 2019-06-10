7373f63bd5a626493aedea00794185a1ea11d3df
KamillKAPLAN <kamilkaplnn@gmail.com>
2019-06-10 14:07:52 +0300

    create-employee.component.html sayfası ve app.module.ts sayfasına FormsModule işlemi yapıldı

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ListEmployeesComponent } from './employees/list-employees.component';
@@ -20,6 +21,7 @@ const appRoutes : Routes = [
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     RouterModule.forRoot(appRoutes)
   ],
   providers: [],
