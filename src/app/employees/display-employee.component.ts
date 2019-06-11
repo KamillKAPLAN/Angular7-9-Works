22f62b098b0ee6cd9bfd89c01b594dafe09c9b60
KamillKAPLAN <kamilkaplnn@gmail.com>
2019-06-11 16:59:52 +0300

    employees(işçi) nextEmployee() metodu yazıldı

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,11 +6,19 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit {
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
   @Input() employee: Employee;
   constructor() { }
 
   ngOnInit() {
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    const previousEmployee = <Employee>changes.employee.previousValue;
+    const currentEmployee = <Employee>changes.employee.currentValue;
+    console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL'));
+    console.log('Current : ' + currentEmployee.name);
+    console.log(changes);
+    
+  }
 }
--- src/app/employees/list-employees.component.ts
+++ src/app/employees/list-employees.component.ts
@@ -9,9 +9,22 @@ import { EmployeeService } from './employee.service';
 export class ListEmployeesComponent implements OnInit {
 
   employees : Employee[];  
+  employeeToDisplay : Employee; 
+  private arrayIndex = 1;
   constructor(private _employeeService : EmployeeService) { }
 
   ngOnInit() {
     this.employees = this._employeeService.getEmployees();
+    this.employeeToDisplay = this.employees[0];
+  }
+
+  nextEmployee(): void{
+    if(this.arrayIndex < this.employees.length) {
+      this.employeeToDisplay = this.employees[this.arrayIndex];
+      this.arrayIndex++;
+    } else {
+      this.employeeToDisplay = this.employees[0];
+      this.arrayIndex = 1;
+    }
   }
 }
