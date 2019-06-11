0426dd34fb918aa6c33b3f58d5b6c35d76661349
KamillKAPLAN <kamilkaplnn@gmail.com>
2019-06-11 17:34:48 +0300

    employees(işçi) ngOnChanges örnek

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges, OnChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,20 +6,34 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit{
+export class DisplayEmployeeComponent implements OnInit, OnChanges {
+  @Input() employeeId: number;
+
   private _employee: Employee;
-  @Input() 
+
+  @Input()
   set employee(val: Employee) {
-    console.log('Previous : ' + (this._employee ? this._employee.name : 'NULL'));
-    console.log('Current : ' + val.name);
     this._employee = val;
   }
   get employee(): Employee {
     return this._employee;
   }
-  
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngOnChanges(changes: SimpleChanges) {
+    for(const propName of Object.keys(changes)) {
+      //console.log(propName);
+      const change = changes[propName];
+      const from = JSON.stringify(change.previousValue);
+      const to = JSON.stringify(change.currentValue);
+
+      console.log(propName + ' changed from ' + from + ' to ' + to);
+      
+      
+    }
   }
 }
