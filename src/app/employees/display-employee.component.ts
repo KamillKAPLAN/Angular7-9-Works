898dfab8907d43d0626ff12db188add52ff0a692
KamillKAPLAN <kamilkaplnn@gmail.com>
2019-06-11 17:19:48 +0300

    employees(işçi) get set özeliği eklendi (display-employee.component.ts)

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Component, OnInit, Input, SimpleChanges } from '@angular/core';
 import { Employee } from '../models/employee.model';
 
 @Component({
@@ -6,19 +6,20 @@ import { Employee } from '../models/employee.model';
   templateUrl: './display-employee.component.html',
   styleUrls: ['./display-employee.component.css']
 })
-export class DisplayEmployeeComponent implements OnInit, OnChanges {
-  @Input() employee: Employee;
+export class DisplayEmployeeComponent implements OnInit{
+  private _employee: Employee;
+  @Input() 
+  set employee(val: Employee) {
+    console.log('Previous : ' + (this._employee ? this._employee.name : 'NULL'));
+    console.log('Current : ' + val.name);
+    this._employee = val;
+  }
+  get employee(): Employee {
+    return this._employee;
+  }
+  
   constructor() { }
 
   ngOnInit() {
-  }
-
-  ngOnChanges(changes: SimpleChanges) {
-    const previousEmployee = <Employee>changes.employee.previousValue;
-    const currentEmployee = <Employee>changes.employee.currentValue;
-    console.log('Previous : ' + (previousEmployee ? previousEmployee.name : 'NULL'));
-    console.log('Current : ' + currentEmployee.name);
-    console.log(changes);
-    
   }
 }
