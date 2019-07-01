7e0c5d10b9d90055798f29982159fa428f855295
KamillKAPLAN <kamilkaplnn@gmail.com>
2019-07-01 17:42:23 +0300

    Angular accordion example, *ngIf="panelExpanded" and panelExpanded=true, [hidden]="isHidden" and isHidden=true

--- src/app/employees/display-employee.component.ts
+++ src/app/employees/display-employee.component.ts
@@ -14,6 +14,7 @@ export class DisplayEmployeeComponent implements OnInit {
   @Input() searchTerm: string;
   @Output() notifyDelete: EventEmitter<number> = new EventEmitter<number>();
   confirmDelete = false;
+  isHidden = true;
 
   constructor(private _route: ActivatedRoute,
               private _router: Router,
